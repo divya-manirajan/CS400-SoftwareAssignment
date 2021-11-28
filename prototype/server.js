2e4e12e0f4ffe047c4c0727b85d77bf4a10d1054
Divya <divya.manirajan@gmail.com>
2021-11-28 15:15:53 -0500

    11/28/21 - Divya - Updated createEvent form to be able to go back to home page - 1 hour

--- prototype/server.js
+++ prototype/server.js
@@ -9,3 +9,19 @@ http.createServer(function(req, res){
     });
 }).listen(8000);
 console.log("Server started on localhost:8000");
+
+http.createServer(function(req, res){
+    fs.readFile("createEvent.html",function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+}).listen(8001);
+
+http.createServer(function(req, res){
+    fs.readFile("createUser.html",function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+}).listen(8002);
