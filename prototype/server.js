435ad955683cf8fd4330c757c22fb29235387931
Divya <divya.manirajan@gmail.com>
2021-11-28 14:07:00 -0500

    11/28/21 - Divya - Updated server to run home.html instead of createEvent.html - 10 minutes

--- prototype/server.js
+++ prototype/server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(req, res){
-    fs.readFile("createEvent.html",function (err, data){
+    fs.readFile("home.html",function (err, data){
         res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
         res.write(data);
         res.end();
